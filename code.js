b964722260e6b11bf49971e1331de83889b3a454
Olcay Açıkdeniz <107851080+olcayacikdeniz@users.noreply.github.com>
2022-07-17 13:18:16 +0300

    Update code.js

--- code.js
+++ code.js
@@ -4,35 +4,22 @@ let ad = document.querySelector("#myName");
 
 ad.innerHTML = Name
 
-let timeDiv = document.querySelector("#myClock");
 
-function showTime() {
+function  time () {
+    let momentary = new Date ();
+
+    let hours = momentary.getHours();
+
+    let minutes = momentary.getMinutes();
+
+    let seconds = momentary.getSeconds();
     
-    const weekday = [
-        "Pazar",
-        "Pazartesi",
-        "Salı",
-        "Çarşamba",
-        "Perşembe",
-        "Cuma",
-        "Cumartesi"
-    ];
-    var time = new Date(),
-    // Access the "getHours" method on the Date object with the dot accessor.
-    hours = time.getHours(),
-    // Access the "getMinutes" method with the dot accessor.
-    minutes = time.getMinutes(),
-    seconds = time.getSeconds(),
-    day = weekday[time.getDay()];
+    let day = new Date ();
 
-  timeDiv.innerHTML = `
-    ${harold(hours)}:${harold(minutes)}:${harold(seconds)} ${day}`;
+    let days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe","Cuma","Cumartesi"]
 
-  function harold(standIn) {
-    if (standIn < 10) {
-      standIn = "0" + standIn;
-    }
-    return standIn;
+    document.querySelector("#myClock").innerHTML = `${hours} : ${minutes} : ${seconds}  ${days[day.getDay()]}`;
+    
   }
-}
-setInterval(showTime, 1000);
+
+  let at_the_moment  = setInterval(time,100);
